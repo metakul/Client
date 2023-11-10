af615b51a11ae0042516835671a0d48469cdf29b
Shubhamkunwar10 <kshubham@c3ihub.ittk.ac.in>
2023-11-10 21:31:57 +0530

    add webvitals

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ export default function App() {
               },
             }}
           />
-          <BrowserRouter>
+          <BrowserRouter basename="/">
             <Router />
           </BrowserRouter>
         </div>
